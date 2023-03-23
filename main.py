eed9b533e8eb260934c8d85c45f133b63b1f51a9
Eobsv <d.eobsv@gamail.com>
2023-03-23 23:56:21 +0100

    Finished project. Going to use it for learing words.

--- main.py
+++ main.py
@@ -29,20 +29,20 @@ def flip_card():
     canvas.itemconfig(card_word, text=current_card["English"], fill="white")
     canvas.itemconfig(card_background, image=card_back_img)
 
+
 def is_known():
     to_learn.remove(current_card)
     print(len(to_learn))
     data = pandas.DataFrame(to_learn)
-    data.to_csv("data/words to learn")
+    data.to_csv("data/words to learn", index=False)
     next_card()
+
 
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-
 flip_timer = window.after(3000, func=flip_card)
-
 
 canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
