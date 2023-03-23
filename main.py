8c780729f229c6205620421dca16aab01ede7d05
Eobsv <d.eobsv@gamail.com>
2023-03-23 23:22:12 +0100

    Buttons now are generating new word in a foreigh language

--- main.py
+++ main.py
@@ -8,10 +8,8 @@ print(to_learn)
 
 def next_card():
     current_card = random.choice(to_learn)
-    french_word = current_card["French"]
-    english_word = current_card["English"]
     canvas.itemconfig(card_title, text="French")
-    canvas.itemconfig(card_word, text="English")
+    canvas.itemconfig(card_word, text=current_card["French"])
 
 
 window = Tk()
@@ -21,8 +19,8 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263, image=card_front_img)
-card_title = canvas.create_text(400, 150, text="Title", font=("Ariel", 48, "italic"))
-card_word = canvas.create_text(400, 263, text="word", font=("Ariel", 52, "bold"))
+card_title = canvas.create_text(400, 150, text="", font=("Arial", 48, "italic"), fill="black")
+card_word = canvas.create_text(400, 263, text="", font=("Arial", 52, "bold"), fill="black")
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(column=0, row=0, columnspan=2)
 
@@ -34,5 +32,6 @@ check_image = PhotoImage(file="images/right.png")
 known_button = Button(image=check_image, highlightthickness=0, command=next_card)
 known_button.grid(row=1, column=1)
 
+next_card()
 
 window.mainloop()
